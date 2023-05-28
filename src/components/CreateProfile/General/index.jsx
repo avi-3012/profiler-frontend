7250c69a56498942f608c28ed6fab6ee1d9c9619
!mute <mute@mutes-MacBook-Air.local>
2023-05-28 23:28:13 +0530

    Update

--- src/components/CreateProfile/General/index.jsx
+++ src/components/CreateProfile/General/index.jsx
@@ -5,6 +5,39 @@ const General = ({ titleState }) => {
   const [male, setMale] = React.useState(false);
   const [female, setFemale] = React.useState(false);
   const [other, setOther] = React.useState(false);
+  const [generalState, setGeneralState] = React.useState({
+    a: "",
+    b: "",
+    c: "",
+    d: "",
+    e: "",
+  });
+
+  React.useEffect(() => {
+    if (
+      localStorage.getItem("general") !== null &&
+      localStorage.getItem("general") !== undefined &&
+      localStorage.getItem("general") !== ""
+    ) {
+      setGeneralState(JSON.parse(localStorage.getItem("general")));
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if (generalState.b !== "") {
+      if (generalState.b === "Male") {
+        setMale(true);
+      } else if (generalState.b === "Female") {
+        setFemale(true);
+      } else if (generalState.b === "Other") {
+        setOther(true);
+      }
+    }
+  }, [generalState]);
+
+  React.useEffect(() => {
+    localStorage.setItem("general", JSON.stringify(generalState));
+  }, [generalState]);
 
   const changeValue = (value) => {
     if (value === "Male") {
@@ -26,7 +59,16 @@ const General = ({ titleState }) => {
     return (
       <div
         className="createprofile-general-body-choice-checked"
-        onClick={() => changeValue(`${value}`)}
+        onClick={() => {
+          if (male) {
+            setGeneralState({ ...generalState, b: "" });
+          } else if (female) {
+            setGeneralState({ ...generalState, b: "" });
+          } else if (other) {
+            setGeneralState({ ...generalState, b: "" });
+          }
+          changeValue(`${value}`);
+        }}
       >
         {value}
       </div>
@@ -37,7 +79,16 @@ const General = ({ titleState }) => {
     return (
       <div
         className="createprofile-general-body-choice-unchecked"
-        onClick={() => changeValue(`${value}`)}
+        onClick={() => {
+          if (!male) {
+            setGeneralState({ ...generalState, b: value });
+          } else if (!female) {
+            setGeneralState({ ...generalState, b: value });
+          } else if (!other) {
+            setGeneralState({ ...generalState, b: value });
+          }
+          changeValue(`${value}`);
+        }}
       >
         {value}
       </div>
@@ -59,6 +110,10 @@ const General = ({ titleState }) => {
             className="createprofile-general-body-input"
             type="text"
             placeholder="Full name"
+            onChange={(e) => {
+              setGeneralState({ ...generalState, a: e.target.value });
+            }}
+            value={generalState.a}
           />
         </div>
         <div className="question">
@@ -83,7 +138,15 @@ const General = ({ titleState }) => {
           <div className="createprofile-general-body-question">
             On which day did you come into this world?
           </div>
-          <input className="createprofile-general-body-input" type="date" />
+          <input
+            className="createprofile-general-body-input"
+            type="date"
+            onChange={(e) => {
+              console.log(generalState);
+              setGeneralState({ ...generalState, c: e.target.value });
+            }}
+            value={generalState.c}
+          />
         </div>
         <div className="question">
           <div className="createprofile-general-body-question">
@@ -93,6 +156,11 @@ const General = ({ titleState }) => {
             className="createprofile-general-body-input"
             type="email"
             placeholder="Your mail"
+            onChange={(e) => {
+              console.log(generalState);
+              setGeneralState({ ...generalState, d: e.target.value });
+            }}
+            value={generalState.d}
           />
         </div>
         <div className="question">
@@ -103,6 +171,11 @@ const General = ({ titleState }) => {
             className="createprofile-general-body-input"
             type="number"
             placeholder="Phone number"
+            onChange={(e) => {
+              console.log(generalState);
+              setGeneralState({ ...generalState, e: e.target.value });
+            }}
+            value={generalState.e}
           />
         </div>
       </div>
